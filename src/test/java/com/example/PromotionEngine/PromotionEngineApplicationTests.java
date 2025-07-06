79a9f4b7c88d5cab1635eba14073d800f04f638d
Tanuja1709 <chennupatitanuja@gmail.com>
2025-07-06 15:10:05 +0530

    added PromotionEngineTest for Scenario A

--- src/test/java/com/example/PromotionEngine/PromotionEngineApplicationTests.java
+++ src/test/java/com/example/PromotionEngine/PromotionEngineApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.PromotionEngine;
 
+import com.example.PromotionEngine.modal.Cart;
+import com.example.PromotionEngine.modal.CartItem
+import com.example.PromotionEngine.modal.SKU;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PromotionEngineApplicationTests {
+import java.util.Arrays;
 
-	@Test
-	void contextLoads() {
+public class PromotionEngineApplicationTests {
+	private PromotionEngine promotionEngine;
+	@BeforeEach
+	public void setup() {
+		SKU A = new SKU("A", 50);
+		SKU B = new SKU("B", 30);
+		SKU C = new SKU("C", 20);
+		SKU D = new SKU("D", 15);
+
+		promotionEngine = new PromotionEngineImpl(Arrays.asList(
+				new BulkPromotion(A, 3, 130),
+				new BulkPromotion(B, 2, 45),
+				new ComboPromotion(C, D, 30)
+		));
 	}
 
+	@Test
+	public void testScenarioA() {
+		Cart cart = new Cart(Arrays.asList(
+				new CartItem(new SKU("A", 50), 1),
+				new CartItem(new SKU("B", 30), 1),
+				new CartItem(new SKU("C", 20), 1)
+		));
+		double total = promotionEngine.calculateTotal(cart);
+		Assertions.assertEquals(100, total);
+	}
 }
